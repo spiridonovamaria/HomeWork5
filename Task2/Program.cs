235b5ea5b1c295326ffb89424e59a86da5b492f5
Maria Spiridonova <traffic2007@mail.ru>
2022-08-25 21:28:38 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+
+
+int[] ArrayFill()
+{
+    System.Console.WriteLine("Массив чисел: ");
+    int length = new Random().Next(3, 10);
+    int[] array1 = new int[length];
+    for (int i = 0; i < array1.Length; i++)
+    {
+        array1[i] = new Random().Next(-25, 25);
+    }
+    return array1;
+}
+
+
+int SumOfOddPositions(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+void ArrayPrint(int[] array)
+{       //Вывод массива в консоль
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+
+
+int[] arr = ArrayFill();
+ArrayPrint(arr);
+int result = SumOfOddPositions(arr);
+System.Console.WriteLine("Сумма элементов нечетных позиций: " + result);
